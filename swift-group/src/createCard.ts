daee3c113e667a61552206a4ae19f267a873e883
PotoVic <159134437+PotoVic@users.noreply.github.com>
2024-12-16 14:06:19 +0100

    add - Script that prints out array object Data in to div box

--- swift-group/src/createCard.ts
+++ swift-group/src/createCard.ts
@@ -43,3 +43,34 @@ const questionList: Questions[] = [
         }
     }    
 ];
+
+function createHtml(questionList: Questions[]): string {
+    let questionListHTML = '';
+
+    questionList.forEach(question => {
+        questionListHTML += `
+        <div class="questionContainer">
+            <div class="questionImageContainer">
+                <img src="${question.image}" alt="${question.alt}" class="questionImage">
+            </div>
+            <div class="questionText">
+                <h2>${question.question}</h2>
+                <ul class="options">
+                    <button>${question.optionAnswer1}</button>
+                    <button>${question.optionAnswer2}</button>
+                    <button>${question.optionAnswer3}</button>
+                </ul>
+            </div>
+        </div>`;
+    });
+    return questionListHTML;
+}
+
+function printHtml(){
+    const htmlContent = createHtml(questionList);
+    const container = document.querySelector<HTMLElement>('.card-container');
+    if (container) {
+        container.innerHTML = htmlContent;
+    }
+}
+printHtml()
