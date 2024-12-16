0a917350c0b794233c69fb8c5b5f5861fea9e028
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-12-16 09:36:38 +0100

    ADD config and FIX dependencies

---
+++ swift-group/eslint.config.js
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser },
+  rules: {
+    camelcase: "error",
+    'comma-dangle': ['error', 'always-multiline'], // Enforce trailing commas in multiline
+    'curly': ['error', 'all'], // Require curly braces for all control statements
+    'no-console': 'warn', // Warn about console.log usage},
+  },},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier
+];
--- swift-group/src/main.ts
+++ swift-group/src/main.ts
@@ -1,4 +1,4 @@
-import './style/style.scss'
+import './style.scss'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
