30b81d473507bac6fc1547f0aa4737e8881c1872
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-12-17 10:09:57 +0100

    FIX eslint.config after feedback

--- swift-group/eslint.config.js
+++ swift-group/eslint.config.js
@@ -1,15 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ['**/*.{js,mjs,cjs,ts}']},
   {languageOptions: { globals: globals.browser },
   rules: {
-    camelcase: "error",
+    camelcase: 'error',
     'comma-dangle': ['error', 'always-multiline'], // Enforce trailing commas in multiline
     'curly': ['error', 'all'], // Require curly braces for all control statements
     'no-console': 'warn', // Warn about console.log usage},
