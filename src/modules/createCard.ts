981afb1ac35ada5f138c18bc4a6755ed7f44d520
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-12-18 09:09:53 +0100

    ADD and UPDATE created a function to check answers

--- src/modules/createCard.ts
+++ src/modules/createCard.ts
@@ -1,60 +1,23 @@
+import { checkAnswer, getScore } from "./checkAnswers";
+import { Questions, questionList } from "./questions";
 
-interface Questions {
-    image: string;
-    alt: string;
-    question: string;
-    optionAnswer1: string;
-    optionAnswer2: string;
-    optionAnswer3: string;
-    answers: {
-        answerOne: boolean;
-        answerTwo: boolean;
-        answerThree: boolean;
-    };
-}
-const questionList: Questions[] = [
-    {
-        image: 'lionking.jpg',
-        alt: 'Lion king',
-        question: 'Who killed Mufasa?',
-        optionAnswer1: 'Simba',
-        optionAnswer2: 'Scar',
-        optionAnswer3: 'Kiara',
-        answers: {
-            answerOne: false,
-            answerTwo: true,
-            answerThree: false,
-        }
-    },
-    {
-        image: 'lionking.jpg',
-        alt: 'Lion king',
-        question: 'Who killed simba?',
-        optionAnswer1: 'Simba',
-        optionAnswer2: 'Scar',
-        optionAnswer3: 'Kiara',
-        answers: {
-            answerOne: false,
-            answerTwo: true,
-            answerThree: false,
-        }
-    },       
-];
 
 let currentQuestionIndex = 0;
+
 function createHtml(questionList: Questions[], index: number){
     const question = questionList[index];
+    const options = Object.keys(question.answer)
+    .map((option) => `<li><button class="option-btn">${option}</button></li>`)
+    .join("")
     return `
         <div class="questionContainer">
             <div class="questionImageContainer">
-                <img src="${question.image}" alt="${question.alt}" class="questionImage">
+                <img src="${question.img}" alt="${question.alt}" class="questionImage">
             </div>
             <div class="questionText">
                 <h2>${question.question}</h2>
                 <ul class="options">
-                    <li><button>${question.optionAnswer1}</button></li>
-                    <li><button>${question.optionAnswer2}</button></li>
-                    <li><button>${question.optionAnswer3}</button></li>
+                ${options}
                 </ul>
             </div>
             <div class="navigationButtons">
@@ -71,12 +34,34 @@ function navigateQuestion() {
         alert('You have reached the end of the questions!');
     }
 }
+
+function handleOptionClick(event: Event){
+    const target = event.target as HTMLElement;
+    if(target.tagName === "BUTTON" && target.classList.contains("option-btn")){
+        const selectedAnswer = target.textContent || "";
+        const currentQuestion = questionList[currentQuestionIndex];
+        const isCorrect = checkAnswer(currentQuestion, selectedAnswer);
+
+        if(isCorrect) {
+            alert("Correct")
+        } else {
+            alert("Wrong")
+        }
+    }
+}
+
 function printHtml() {
     const htmlContent = createHtml(questionList, currentQuestionIndex);
     const container = document.querySelector<HTMLElement>('.card-container');
     if (container) {
         container.innerHTML = htmlContent;
     }
+
+    const optionButton = document.querySelectorAll(".option-btn");
+    optionButton.forEach((button) => {
+        button.addEventListener("click", handleOptionClick)
+    })
+
     const nextButton = document.getElementById('nextButton');
     if (nextButton) {
         nextButton.addEventListener('click', navigateQuestion);
