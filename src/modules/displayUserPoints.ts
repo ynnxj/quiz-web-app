56c6b1e0351e320edccd8b5bf055e0a17cc6923a
JanisSeibutis <janis.seibutis@gmail.com>
2024-12-18 16:23:10 +0100

    Add points display & add styling to its parent div

--- src/modules/displayUserPoints.ts
+++ src/modules/displayUserPoints.ts
@@ -1,2 +1,7 @@
 import { userScore } from "./checkAnswers";
 
+const userPoints: HTMLElement | null = document.querySelector('#points-btn')
+
+if (userPoints) {
+    userPoints.textContent = `${userScore}pts`
+}
