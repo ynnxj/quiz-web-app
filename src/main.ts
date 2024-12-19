d742426bfd63fd9b213feda653939828b9ab5ecc
PotoVic <159134437+PotoVic@users.noreply.github.com>
2024-12-19 15:28:34 +0100

    fix - Fixed error, Was calling the wrong Function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './styles/styles.scss'
-import { createHtml } from './modules/createCard'
+import { printHtml } from './modules/createCard'
 import { displayUserPoints } from './modules/displayUserPoints'
 
-createHtml()
+printHtml()
 displayUserPoints()
