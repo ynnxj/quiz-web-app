b0f006d1ed32a6f7dedff64e769d902d452e760d
JanisSeibutis <janis.seibutis@gmail.com>
2024-12-19 13:10:12 +0100

    Fix displayUserpoints if rules

--- src/modules/displayUserPoints.ts
+++ src/modules/displayUserPoints.ts
@@ -3,7 +3,7 @@ import { userScore } from "./checkAnswers";
 const userPoints: HTMLElement | null = document.querySelector('#points-btn')
 
 export const displayUserPoints = () => {
-    if (userPoints) {
+    if (userPoints !== null) {
         userPoints.textContent = `${userScore}pts`
     }
 }
